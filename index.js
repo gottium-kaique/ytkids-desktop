d773ca8a924c4b1c936f3cb61d6e7d1105aa4bbb
gottiumkaique <65359117+gottiumkaique@users.noreply.github.com>
2020-09-03 15:29:25 -0300

    Arquivo principal do projeto

---
+++ index.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require("electron")
+
+function createWindow() {
+  const Window = new BrowserWindow({
+    minWidth: 1100,
+    minHeight: 750,
+    height: 750,
+    width: 1100,
+    title: "Youtube Kids",
+    show: false,
+    autoHideMenuBar: true,
+    center: true,
+    backgroundColor: "#334",
+    icon: "icon.png",
+  })
+
+  Window.loadURL("https://youtubekids.com/").catch(() => {
+    Window.loadFile("connection-error.html")
+  })
+
+  Window.on("ready-to-show", () => {
+    Window.show()
+  })
+}
+
+app.whenReady().then(createWindow)
