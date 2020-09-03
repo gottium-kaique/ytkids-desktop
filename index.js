25097a8c06aacc1f99729c2b8c0ec928f76458f5
gottium-kaique <69827216+gottium-kaique@users.noreply.github.com>
2020-09-03 19:38:26 -0300

    Arquivo principal

--- index.js
+++ index.js
@@ -24,3 +24,17 @@ function createWindow() {
 }
 
 app.whenReady().then(createWindow)
+
+app.on("window-all-closed", () => {
+ 
+  if (process.platform !== "darwin") {
+    app.quit()
+  }
+})
+
+app.on("activate", () => {
+  
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
